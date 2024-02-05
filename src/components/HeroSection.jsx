2f1fc2c09af6d78fdeaf95110b363ef4ec9aeca0
Mickey254-ja <mickey.awino@student.moringaschool.com>
2024-02-05 15:54:02 +0300

    🍱adds assets and 🩹fixes styling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
        <Row className='h-100'>
         <Col xs md="8" className='d-flex flex-column justify-content-center text-start px-4'>
             <div className='mx-4'>
-               <h1>Photography Portfolio and My Project</h1>
+               <h1>Edu Photography</h1>
                <p>lprem ipsum sit amet, cosectetura dipiscing elit, sed do elusmod tekmpor incididunt ut labore mana aliqua, Ut enim ad minim venam, quis nostrud exercitation ulianci laboris nisi ut aliquip ex ea commodo consequier.</p>
             </div>
             <div className='btns-wrapper'>
--- src/components/MenuOverlay.jsx
+++ src/components/MenuOverlay.jsx
@@ -5,13 +5,13 @@ export const MenuOverlay = ({ navbarOpen, setNavbarOpen }) => {
     <nav className={`menu-container ${navbarOpen ? "expanded" : ""}`}>
         <ul className='menu-list'>
             <li className='nav-li'>
-               <a href='/' className='nav-link' onClick={() => setNavbarOpen(false)}>
+               <a href="/" className='nav-link' onClick={() => setNavbarOpen(false)}>
                 Home
                 </a>
             </li>
 
             <li className='nav-li'>
-               <a href='/' className='nav-link' onClick={() => setNavbarOpen(false)}>
+               <a href="/" className='nav-link' onClick={() => setNavbarOpen(false)}>
                 About
                 </a>
             </li>
