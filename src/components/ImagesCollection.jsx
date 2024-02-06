3bc2505b22b4472a3b5f5292a025b1943a3afa5f
Mickey254-ja <mickey.awino@student.moringaschool.com>
2024-02-06 15:03:57 +0300

    adds styling

--- src/components/ImagesCollection.jsx
+++ src/components/ImagesCollection.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 
 const ImagesCollection = () => {
-    const imagesList = [...new Array(9)].map(
+    const imagesList = [...new Array(8)].map(
         (number, index) => `/images/image-${index + 1}.jpg`
         );
 
   return (
-    <ul className='image-collections-container'>
-    {imagesList.map((imgUrl, index) => {
-    console.log(imgUrl); // Log the image URL to the console
-    return (
-        <li className="item" key={index}>
+    <>
+      <ul className='image-collections-container'>
+        {imagesList.map((imgUrl, index) => {
+          return (
+          <li className={index % 2 === 0 ? "item short" : 
+          "item tall"} >
             <figure>
-                <img src={imgUrl} alt={`collection-${index + 1}`} />
+                <img src={imgUrl} alt="collection" />
             </figure>
         </li>
     );
 })}
     </ul>
+    <div className='d-flex nb-2'>
+        <a href='/' className='mx-auto fs-3'>
+            View More
+        </a>
+    </div>
+    </>
   );
 }
 
