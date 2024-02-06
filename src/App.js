0a24fe546e22ce4f68d0fe31e0c2e5ef47031c3a
Mickey254-ja <mickey.awino@student.moringaschool.com>
2024-02-06 12:11:31 +0300

    🔥removes files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import HeroSection from './components/HeroSection';
 import { Container } from 'react-bootstrap';
 import { Header } from './components/Header';
 import { MenuOverlay }from './components/MenuOverlay';
-// import ResponsiveImage from './components/ResponsiveImage';
 import ImagesCollection from './components/ImagesCollection';
 import { useState } from 'react';
 
@@ -15,13 +14,6 @@ function App() {
       <Header navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
       <MenuOverlay navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
        <HeroSection />
-       {/* <ResponsiveImage
-      originalSrc="path-to-original-image.jpg"
-      smallSrc="path-to-small-image.jpg"
-      mediumSrc="path-to-medium-image.jpg"
-      largeSrc="path-to-large-image.jpg"
-      altText="Description"
-    /> */}
        <ImagesCollection />
      </Container>
     </div>
